5b237bd285294f2931717d1afe7182343d2a0289
Francisco Wallison <franciscowallison@gmail.com>
2017-03-20 14:59:27 -0300

    variaveis sobre escrever

--- aulas/aula01/hello.php
+++ aulas/aula01/hello.php
@@ -3,6 +3,22 @@
  *  tag  	<b> foi depreciada
  *  tag 	<strong> melhoria de semantica e leitura para cegos 
  */
-	//echo "<strong>Hello World!!</strong>";
-	phpinfo();
+	echo "<strong>Hello World!!!</strong>";
+	//phpinfo();
+
+	echo "\n";
+
+	echo '<br>';
+	
+	$nome = "Wallison";
+
+	echo $nome;
+
+	echo '<br>';
+
+	$nome = "Kleber";
+
+	echo $nome;
+
+
 ?>
