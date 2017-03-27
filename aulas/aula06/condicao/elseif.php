c739abb8e9c3df0ec28dfd328a7492e4c737440d
franciscoWallison <franciscowallison@gmail.com>
2017-03-27 15:26:23 -0300

    aula06

---
+++ aulas/aula06/condicao/elseif.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Estrutura de condição emcadiada
+ */
+
+echo '<hr>';
+
+$mes =  rand(1,5) ;
+
+	if( $mes == 1 )
+	{
+		echo "Janeiro";
+	}elseif($mes == 2)
+	{
+		echo "Fevereiro";
+	}elseif($mes == 3)
+	{
+		echo "Março";
+	}elseif($mes == 4)
+	{
+		echo "Abril";
+	}elseif($mes == 5)
+	{
+		echo "Maio";
+	}
---
+++ aulas/aula06/repedicao/test.php
@@ -0,0 +1,11 @@
+<?php 
+
+	for($n=100;$n==0;$n=$n-3){
+
+		if($n == 0){
+			echo "nada";
+		}else{
+			echo $n;
+		};
+
+	};
